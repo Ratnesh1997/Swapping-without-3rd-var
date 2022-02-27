25f20cf3d8a7eec045bb30fb022ce17ebfda2bbb
Administrator <Administrator@iZa2d0rkybaz2oZ>
2022-02-27 14:27:00 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swapping_without_3rd_var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        
+        int x = 10, y = 5;
+
+            Console.WriteLine("program to swap two number without using third variable");
+        x = x* y; 
+        y = x / y; 
+    x = x / y; 
+            Console.WriteLine("after swapping{0},{1}", x, y);
+            Console.ReadKey();
+}
+}
+}
